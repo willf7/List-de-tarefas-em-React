a82b7dd910ce3c4e6eae7ddd28b2cd0df684e182
William Felipe <williamfelipe081@gmail.com>
2023-10-20 15:04:06 -0300

    Finalizado primeira parte do layout

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./Main.css";
+
+import { FaPlus } from "react-icons/fa";
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de Tarefas</h1>
+
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <>
+              <FaPlus />
+            </>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
